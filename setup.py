9463aef9fbbb5585643cf324e78edfbc1344accc
Nicolas Noé <nicolas@niconoe.org>
2012-10-14 16:09:05 +0200

    Preparing for distribution.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-nuages-tag',
-    version='0.1.1',
+    version='0.1.2',
     author=u'Nicolas Noé',
     author_email='nicolas@niconoe.eu',
     packages=find_packages(),
