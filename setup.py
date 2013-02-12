327ba6358d50f07ab72a461b165d618a1599684b
Nicolas Noé <nicolas@niconoe.org>
2013-02-12 21:48:10 +0100

    Prepare for distribution.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 
 setup(
     name='django-nuages-tag',
-    version='0.1.2',
+    version='0.1.3',
     author=u'Nicolas Noé',
     author_email='nicolas@niconoe.eu',
     packages=find_packages(),
     url='https://github.com/niconoe/django-nuages-tag/',
     license='BSD licence, see LICENCE.txt',
-    description='A simple Django TemplateTag (named compute_tag_cloud) to help in the creation of tag clouds.',
+    description='A simple Django TemplateTag (compute_tag_cloud) to help in the creation of tag clouds.',
     long_description=open('README.rst').read(),
     include_package_data=True,
     zip_safe=False,
